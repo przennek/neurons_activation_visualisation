2521943e431f5716a4067427f7727c269f3b7131
Przemyslaw Chlipala <pchlipal@akamai.com>
2019-09-10 20:43:14 +0200

    RC

--- src/nst.py
+++ src/nst.py
@@ -15,9 +15,9 @@ parser.add_argument('style_reference_image_path', metavar='ref', type=str,
                     help='Path to the style reference image.')
 parser.add_argument('result_prefix', metavar='res_prefix', type=str,
                     help='Prefix for the saved results.')
-parser.add_argument('--iter', type=int, default=10, required=False,
+parser.add_argument('--iter', type=int, default=40, required=False,
                     help='Number of iterations to run.')
-parser.add_argument('--content_weight', type=float, default=0.025, required=False,
+parser.add_argument('--content_weight', type=float, default=0.005, required=False,
                     help='Content weight.')
 parser.add_argument('--style_weight', type=float, default=1.0, required=False,
                     help='Style weight.')
